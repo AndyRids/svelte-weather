c3bf242d8ad229f6d9358966b6896cc852cb935c
Andrew Ridyard <ridyard12@gmail.com>
2021-02-01 17:11:43 +0000

    initial view component commit

---
+++ src/components/views/MainView.svelte
@@ -0,0 +1,27 @@
+<script>
+  // components
+  import SearchLocation from '../search/SearchLocation.svelte';
+  import Weather from '../weather/Weather.svelte';
+
+  // current displayed component name
+  let componentName = 'Weather';
+
+  // reference to imported weather components
+  const components = { Weather, SearchLocation };
+
+</script>
+
+<section class="main-view">
+
+    <svelte:component this={components[componentName]} bind:componentName />
+
+</section>
+
+<style>
+  .main-view {
+    grid-column: 2 / 3;
+    height: 100%;
+    position: relative;
+    width: 100%;
+  }
+</style>
